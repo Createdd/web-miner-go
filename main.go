7c4578ca57ca18982ae698e2a27672b4971c4513
Daniel Deutsch <deudan1010@gmail.com>
2018-08-10 17:17:47 +0200

    #1 Initalize a repo that is working

--- main.go
+++ main.go
@@ -1,69 +1,71 @@
 package main
 
 import (
-	"database/sql"
+	// "database/sql"
 	"fmt"
-	"log"
-	"net/url"
-	"os"
-	"time"
+	// "log"
+	// "net/url"
+	// "os"
+	// "time"
 
-	httpDeliver "github.com/bxcodec/go-clean-arch/article/delivery/http"
-	articleRepo "github.com/bxcodec/go-clean-arch/article/repository"
-	articleUcase "github.com/bxcodec/go-clean-arch/article/usecase"
-	_authorRepo "github.com/bxcodec/go-clean-arch/author/repository"
-	"github.com/bxcodec/go-clean-arch/middleware"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/labstack/echo"
-	"github.com/spf13/viper"
+	// httpDeliver "github.com/bxcodec/go-clean-arch/article/delivery/http"
+	// articleRepo "github.com/bxcodec/go-clean-arch/article/repository"
+	// articleUcase "github.com/bxcodec/go-clean-arch/article/usecase"
+	// _authorRepo "github.com/bxcodec/go-clean-arch/author/repository"
+	// "github.com/bxcodec/go-clean-arch/middleware"
+	// _ "github.com/go-sql-driver/mysql"
+	// "github.com/labstack/echo"
+	// "github.com/spf13/viper"
 )
 
-func init() {
-	viper.SetConfigFile(`config.json`)
-	err := viper.ReadInConfig()
+// func init() {
+// 	viper.SetConfigFile(`config.json`)
+// 	err := viper.ReadInConfig()
 
-	if err != nil {
-		panic(err)
-	}
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	if viper.GetBool(`debug`) {
-		fmt.Println("Service RUN on DEBUG mode")
-	}
+// 	if viper.GetBool(`debug`) {
+// 		fmt.Println("Service RUN on DEBUG mode")
+// 	}
 
-}
+// }
 
 func main() {
 
-	dbHost := viper.GetString(`database.host`)
-	dbPort := viper.GetString(`database.port`)
-	dbUser := viper.GetString(`database.user`)
-	dbPass := viper.GetString(`database.pass`)
-	dbName := viper.GetString(`database.name`)
-	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
-	val := url.Values{}
-	val.Add("parseTime", "1")
-	val.Add("loc", "Asia/Jakarta")
-	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
-	dbConn, err := sql.Open(`mysql`, dsn)
-	if err != nil && viper.GetBool("debug") {
-		fmt.Println(err)
-	}
-	err = dbConn.Ping()
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
+	fmt.Printf("test")
 
-	defer dbConn.Close()
-	e := echo.New()
-	middL := middleware.InitMiddleware()
-	e.Use(middL.CORS)
-	authorRepo := _authorRepo.NewMysqlAuthorRepository(dbConn)
-	ar := articleRepo.NewMysqlArticleRepository(dbConn)
+	// dbHost := viper.GetString(`database.host`)
+	// dbPort := viper.GetString(`database.port`)
+	// dbUser := viper.GetString(`database.user`)
+	// dbPass := viper.GetString(`database.pass`)
+	// dbName := viper.GetString(`database.name`)
+	// connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	// val := url.Values{}
+	// val.Add("parseTime", "1")
+	// val.Add("loc", "Asia/Jakarta")
+	// dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	// dbConn, err := sql.Open(`mysql`, dsn)
+	// if err != nil && viper.GetBool("debug") {
+	// 	fmt.Println(err)
+	// }
+	// err = dbConn.Ping()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// 	os.Exit(1)
+	// }
 
-	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
-	au := articleUcase.NewArticleUsecase(ar, authorRepo, timeoutContext)
-	httpDeliver.NewArticleHttpHandler(e, au)
+	// defer dbConn.Close()
+	// e := echo.New()
+	// middL := middleware.InitMiddleware()
+	// e.Use(middL.CORS)
+	// authorRepo := _authorRepo.NewMysqlAuthorRepository(dbConn)
+	// ar := articleRepo.NewMysqlArticleRepository(dbConn)
 
-	e.Start(viper.GetString("server.address"))
+	// timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
+	// au := articleUcase.NewArticleUsecase(ar, authorRepo, timeoutContext)
+	// httpDeliver.NewArticleHttpHandler(e, au)
+
+	// e.Start(viper.GetString("server.address"))
 }
