d13b70f5704478d67ebe7a4461160df1108cfde4
Daniel Deutsch <deudan1010@gmail.com>
2018-08-11 16:59:36 +0200

    #12 Get information with http and display json string, first working version

--- main.go
+++ main.go
@@ -1,78 +1,49 @@
 package main
 
 import (
-	// "github.com/gin-gonic/gin"
+	"fmt"
+	"github.com/gin-gonic/gin"
 	// "fmt"
-	medium "github.com/medium/medium-sdk-go"
-	"log"
+	"net/http"
+	"io/ioutil"
+	"strings"
+	// "log"
 )
 
 func main() {
-	// router := gin.Default()
-	// router.GET("/", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"test": "works",
-	// 	})
-	// })
-	// router.GET("/json", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"json": "works",
-	// 	})
-	// })
-	// router.Run(":3000")
+
 	// fmt.Println("_______________________")
 	// fmt.Println("hello")
 
-	// Go to https://medium.com/me/applications to get your applicationId and applicationSecret.
-	m := medium.NewClient("YOUR_APPLICATION_ID", "YOUR_APPLICATION_SECRET")
-
-	// Build the URL where you can send the user to obtain an authorization code.
-	url := m.GetAuthorizationURL("secretstate", "https://yoursite.com/callback/medium",
-        medium.ScopeBasicProfile, medium.ScopePublishPost)
-
-	// (Send the user to the authorization URL to obtain an authorization code.)
-
-	// Exchange the authorization code for an access token.
-	at, err := m.ExchangeAuthorizationCode("YOUR_AUTHORIZATION_CODE", "https://yoursite.com/callback/medium")
+	resp, err := http.Get("https://medium.com/@ddcreationstudi/latest?format=json")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
+	defer resp.Body.Close()
 
-	// The access token is automatically set on the client for you after
-	// a successful exchange, but if you already have a token, you can set it
-	// directly.
-	m.AccessToken = at.AccessToken
-
-	// If you have a self-issued access token, you can skip these steps and
-	// create a new client directly:
-	m2 := medium.NewClientWithAccessToken("SELF_ISSUED_ACCESS_TOKEN")
-
-	// Get profile details of the user identified by the access token.
-	// Empty string mean current user, otherwise you need to indicate
-	// the user id (alphanumeric string with 65 chars)
-	u, err := m2.GetUser("")
-	if err != nil {
-		log.Fatal(err)
+	body, err2 := ioutil.ReadAll(resp.Body)
+	if err2 != nil {
+		fmt.Println(err2)
 	}
+	data := string(body)
 
-	// Create a draft post.
-	p, err := m.CreatePost(medium.CreatePostOptions{
-		UserID:        u.ID,
-		Title:         "Title",
-		Content:       "<h2>Title</h2><p>Content</p>",
-		ContentFormat: medium.ContentFormatHTML,
-		PublishStatus: medium.PublishStatusDraft,
+	convertedData := strings.Replace(data, "])}while(1);</x>", "", 1)
+
+	fmt.Print(string(convertedData))
+	fmt.Print("_________________")
+	fmt.Print(err, err2)
+
+
+	// Set up router
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"test": "works",
+		})
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
+	router.GET("/json", func(c *gin.Context) {
+		c.String(200, convertedData)
+	})
+	router.Run(":3000")
 
-	// When your access token expires, use the refresh token to get a new one.
-	nt, err := m.ExchangeRefreshToken(at.RefreshToken)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Confirm everything went ok. p.URL has the location of the created post.
-	log.Println(url, at, u, p, nt)
 }
